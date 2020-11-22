ff12d8943c36647e93e3c4bc6af2dfb61e717d8e
cmcfadzean <court@kajabi.com>
2020-11-21 19:30:18 -0800

    adding ideas

---
+++ app/views/ideas/_idea.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! idea, :id, :title, :subtitle, :body, :user_id, :created_at, :updated_at
+json.url idea_url(idea, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_22_043955) do
+ActiveRecord::Schema.define(version: 2020_11_22_032454) do
+
+  create_table "ideas", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_ideas_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
